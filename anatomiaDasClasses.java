b884e17fb1280cf28a2eca480c8f5c348c7d3252
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-03-13 20:12:33 -0300

    Conclusão de "Anatomia das Classes"

---
+++ anatomiaDasClasses.java
@@ -0,0 +1,17 @@
+// Toda aplicação Java é organizada em classes. 
+// Uma classe pode conter atributos (variáveis), métodos (funções) e Contrutores.
+
+// Exemplo abaixo de classes:
+
+public class anatomiaDasClasses {
+    
+    // Atributo de classe:
+        String nome;
+
+    // Método principal:
+        public static void main(String[] args) {
+            
+            System.out.println("Olá programador, seja bem vindo!");       }
+
+    
+}
