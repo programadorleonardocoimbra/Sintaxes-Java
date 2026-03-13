7f9bd7e70018d9edfb818a127c0151879fa5d93c
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-03-13 20:53:57 -0300

    Conclusão de "operadores"

---
+++ operadores.java
@@ -0,0 +1,20 @@
+// Os operadores realizam operações matemáticas ou lógicas.
+
+public class operadores {
+    public static void main(String[] args) {
+     
+        int numero1 = 10;
+        int numero2 = 5;
+
+        int soma = numero1 + numero2;
+        int subtracao = numero1 - numero2;
+        int multiplicacao = numero1 * numero2;
+        int divisao = numero1 / numero2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+
+    }    
+}
