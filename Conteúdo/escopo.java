2d12fc347f1278ef419156b731e8cf8d95a30a0c
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-03-13 21:21:54 -0300

    Conclusão de "escopo e palavras reservadas"

---
+++ Conteúdo/escopo.java
@@ -0,0 +1,26 @@
+package Conteúdo;
+/*
+Escopo define onde uma variável pode ser utilizada.
+Tipos de escopo:
+
+> Escopo de classe
+> Escopo de método
+> Escopo de bloco
+*/
+
+public class escopo {
+    static int numero = 10; // variável global de classe.
+
+    public static void main(String[] args) {
+
+        int valor = 5; // variável local.
+
+        if (valor > 0) {
+            int dentroDoIf = 20;
+            System.out.println(dentroDoIf);
+            
+        }
+
+
+    }
+}
---
+++ Conteúdo/palavrasReservadas.java
@@ -0,0 +1,15 @@
+package Conteúdo;
+
+// Java possui palavras que não podem ser usadas como nomes de variáveis ou classes.
+// Segue palavras: class, public, private, protected, static, void, void, int, double, if, else, while, for, return, new, etc.
+
+public class palavrasReservadas {
+
+int class = 10; // Erro de Sintaxe, palavra class não pode utilizada como variável ou classe
+                
+// Substitua por:
+
+int numeroClasse = 10;
+
+    
+}
