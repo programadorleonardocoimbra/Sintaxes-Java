0d988d285c48a58307cdada5819fb0813a3abe8e
Leonardo Coimbra <programadorleonardocoimbra@gmail.com>
2026-03-13 21:24:44 -0300

    Conclusão de "javaDoc"

---
+++ Conteúdo/javaDoc.java
@@ -0,0 +1,17 @@
+package Conteúdo;
+/**
+ * Classe responsável por realizar operações matemáticas.
+ */
+public class javaDoc {
+
+    /**
+     * Realiza a soma de dois números.
+     * 
+     * @param a primeiro número
+     * @param b segundo número
+     * @return resultado da soma
+     */
+    public int somar(int a, int b) {
+        return a + b;
+    }
+}
